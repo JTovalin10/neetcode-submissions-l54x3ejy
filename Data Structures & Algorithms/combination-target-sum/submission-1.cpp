1d4301289f4843690c8e12f73bf877e456f31693
Justin Hernandez Tovalin <justinhernlin@icloud.com>
2026-04-11 23:27:52 -0700

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+vector<vector<int>> res{};
+    vector<vector<int>> combinationSum(const vector<int>& nums, int target) {
+        vector<int> curr;
+        backtrack(nums, target, curr, 0);
+        return res;
+    }
+
+private:
+    void backtrack(const vector<int>& nums, int target, vector<int>& curr, int i) {
+        if (target == 0) {
+            res.push_back(curr);
+            return;
+        }
+
+        if (target < 0 || i >= nums.size()) return;
+        // unbounded knapsack approach
+        curr.push_back(nums[i]);
+        backtrack(nums, target - nums[i], curr, i);
+        curr.pop_back();
+        backtrack(nums, target, curr, i + 1);
+    }
+};
