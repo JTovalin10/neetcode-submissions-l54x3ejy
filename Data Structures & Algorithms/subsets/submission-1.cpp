21804fcefe0e2541b71d48bdcb743abe9ff253e2
Justin Hernandez Tovalin <justinhernlin@icloud.com>
2026-04-11 23:10:57 -0700

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        sub(nums, 0, {});
+        return {res.begin(), res.end()};
+    }
+private:
+set<vector<int>> res{};
+
+void sub(const vector<int>& nums, int index, vector<int> curr) {
+    if (index > nums.size()) return;
+    //if (res.count(curr)) return;
+    res.insert(curr);
+    
+    sub(nums, index + 1, curr);
+    curr.push_back(nums[index]);
+    sub(nums, index + 1, curr);
+    curr.pop_back();
+}
+};
