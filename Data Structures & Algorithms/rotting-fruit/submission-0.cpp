0ec5ca2ea0cff7620d26267ced1179f61f175db9
Justin Hernandez Tovalin <justinhernlin@icloud.com>
2026-04-12 22:57:54 -0700

    Add: rotting-fruit - submission-0

---
+++ Data Structures & Algorithms/rotting-fruit/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        // bfs approach as we need to do it layer by layer to handle concurrent rotting
+        int ROWS = grid.size();
+        int COLS = grid[0].size();
+        int fresh = 0;
+        queue<pair<int, int>> q;
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                if (grid[i][j] == 2) q.push({i, j});
+                if (grid[i][j] == 1) fresh++;
+            }
+        }
+        vector<pair<int, int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int time = 0;
+        while (!q.empty() && fresh > 0) {
+            int size = q.size();
+            for (int i = 0; i < size && fresh > 0; i++) {
+                auto [x, y] = q.front(); q.pop();
+                for (auto [x2, y2] : dir) {
+                    int dx = x + x2;
+                    int dy = y + y2;
+                    if (dx < 0 || dy < 0 || dx >= ROWS || dy >= COLS) continue;
+                    if (grid[dx][dy] != 1) continue;
+                    grid[dx][dy] = 2; // make it rotten
+                    q.push({dx, dy});
+                    fresh--;
+                }
+            }
+            if (!q.empty()) time++;
+        }
+        return fresh == 0 ? time : -1;
+    }
+};
