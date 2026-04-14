50f62a0e8b9b3dd3ce9c507b670844b27eb99bbc
Justin Hernandez Tovalin <justinhernlin@icloud.com>
2026-04-13 21:30:12 -0700

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+private:
+unordered_map<int, int> memo{};
+
+public:
+    int climbStairs(int n) {
+        if (n < 0) return 0;
+        if (n == 0) return 1;
+        if (memo.contains(n)) return memo[n];
+        memo[n] =  climbStairs(n - 1) + climbStairs(n - 2);
+        return memo[n];
+    }
+};
