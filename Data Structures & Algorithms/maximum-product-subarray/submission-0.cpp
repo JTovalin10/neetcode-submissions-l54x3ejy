2c1816eaf408db6fed2725f1c73b76142c9bb15e
Justin Hernandez Tovalin <justinhernlin@icloud.com>
2026-04-13 23:46:27 -0700

    Add: maximum-product-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int res = nums[0];
+        int currMin = 1, currMax = 1;
+        for (int num : nums) {
+            int tmp = currMax * num; // store for min
+            currMax = max(max(currMax * num, currMin * num), num);
+            currMin = min(min(tmp, currMin * num), num);
+            res = max(res, currMax);
+        }
+        return res;
+    }
+};
