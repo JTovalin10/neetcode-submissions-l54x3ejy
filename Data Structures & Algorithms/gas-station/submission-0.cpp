5cc45268e55c560f6fff5f0088b1ef03cb975d1c
Justin Hernandez Tovalin <justinhernlin@icloud.com>
2026-04-13 19:47:45 -0700

    Add: gas-station - submission-0

---
+++ Data Structures & Algorithms/gas-station/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        if (accumulate(gas.begin(), gas.end(), 0) < accumulate(cost.begin(), cost.end(), 0)) return -1;
+
+        int res = 0, curr = 0;
+        for (int i = 0; i < cost.size(); i++) {
+            curr += (gas[i] - cost[i]);
+            if (curr < 0) {
+                curr = 0;
+                res = i + 1;
+            }
+        }
+        return res;
+    }
+};
